140133352108bd10d47d6c3d096c0b8df97a2971
ALowton <ALowton@QA.com>
2022-06-14 15:16:10 +0100

    AL: DT exercise

--- src/com/qa/datatypes/DataTypes.java
+++ src/com/qa/datatypes/DataTypes.java
@@ -1,32 +1,42 @@
 package com.qa.datatypes;
 
 public class DataTypes {
-
-//	DataType variableName = value;
-
-//  non-primitive (object) datatypes (start with capital letters)
-	static String myString = "Text";
-	
-//	primitive datatypes (lowercase)
-	static int age;
-	
-// 	float and double are used for decimal numbers	
-//	float bankBalance = 4999.99f;
-	static double bankBalance = 4999.99;
-	
-//	boolean values are either true or false
-	static boolean happy = true;
-	
-
-//  characters are single unicode characters
-	static char letter = 'a';
-
-//	Methods can return any datatype, including void (meaning returns nothing)
-	public static String returnsSomething() {
-		return "hello";
-	}
 	
 	public static void main(String[] args) {
-		System.out.println(happy);
+		System.out.println(methodOne());
+		System.out.println(methodTwo());
+		System.out.println(methodThree());
+		System.out.println(methodFour());
+		System.out.println(methodFive());
+		System.out.println(methodSix());
+		methodSeven(); // This is void, and will print on it's own
+	}
+	
+	public static int methodOne() {
+		return 0;
+	}
+	
+	public static boolean methodTwo() {
+	    return true;
+	}
+	
+	public static char methodThree() {
+	    return 'P';
+	}
+	
+	public static long methodFour() {
+	    return 52L;
+	}
+	
+	public static float methodFive() {
+	    return 0.6f;
+	}
+	
+	public static String methodSix() {
+	    return "Hi there";
+	}
+	
+	public static void methodSeven() {
+	   	System.out.println("I'm void!");
 	}
 }
