25947c870b6d2002bffbe602b4b49f996cf2c9d6
ALowton <ALowton@QA.com>
2022-06-14 10:14:40 +0100

    AL: initial commit

---
+++ src/com/qa/helloworld/HelloWorld.java
@@ -0,0 +1,20 @@
+package com.qa.helloworld;
+
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		printMessage();
+	}
+
+	
+	public static void printMessage() {
+//		DataType variableName = value;
+		String myMessage = "Hello world";
+		
+//		println is used to print text to new lines
+		System.out.println(myMessage);
+		
+//		print is used to print text to the same line
+//		System.out.print("Hello World");
+	}
+}
