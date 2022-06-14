410d493357acd4e9e06af83aaf2c0b87c25d3b12
ALowton <ALowton@QA.com>
2022-06-14 12:11:42 +0100

    AL: added helloworld

--- src/com/qa/helloworld/HelloWorld.java
+++ src/com/qa/helloworld/HelloWorld.java
@@ -3,18 +3,26 @@ package com.qa.helloworld;
 public class HelloWorld {
 	
 	public static void main(String[] args) {
-		printMessage();
+//		printMessage("This is my message!");
+//		printMessage("A different message");
+		
+		String result = returnString("Original Value");
+		System.out.println(result);
 	}
 
 	
-	public static void printMessage() {
+	public static void printMessage(String myMessage) {
 //		DataType variableName = value;
-		String myMessage = "Hello world";
+//		String myMessage = "Hello world";
 		
 //		println is used to print text to new lines
 		System.out.println(myMessage);
 		
 //		print is used to print text to the same line
 //		System.out.print("Hello World");
+	}
+	
+	public static String returnString(String myMessage) {
+		return myMessage + " I've added something to this!!!";
 	}
 }
