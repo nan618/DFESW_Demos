d24103ecada4cac0b30e8383b0fe69e5d5942158
jennifer.godman@udsl.com <jennifer.godman@udsl.com>
2022-06-15 20:19:53 +0100

    QA honework

---
+++ src/com/qa/calculate/Calculator.java
@@ -0,0 +1,20 @@
+package com.qa.calculate;
+
+public class Calculator {
+	
+	public int add(int numOne, int numTwo) {
+		return numOne + numTwo;
+	}
+	
+	public int multipli(int numOne, int numTwo) {
+		return numOne * numTwo;
+	}
+	
+	public double divide(double numOne, double numTwo) {
+		return numOne / numTwo;
+	}
+	
+	public int subtract(int numOne, int numTwo) {
+		return numOne - numTwo;
+	}
+}
---
+++ src/com/qa/calculate/CalculatorTest.java
@@ -0,0 +1,12 @@
+package com.qa.calculate;
+
+public class CalculatorTest {
+
+	public static void main(String[] args) {
+		Calculator calculator = new Calculator();
+		
+		System.out.println(calculator.divide(5, 3));
+
+	}
+
+}
