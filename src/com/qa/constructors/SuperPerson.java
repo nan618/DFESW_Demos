16c4e3525ab964e15ff3ffb43ca08d3f93d4830b
jennifer.godman@udsl.com <jennifer.godman@udsl.com>
2022-06-15 11:22:39 +0100

    SupperPerson created

---
+++ src/com/qa/constructors/SuperPerson.java
@@ -0,0 +1,20 @@
+package com.qa.constructors;
+
+public class SuperPerson extends Person {
+ 
+	private static SuperPerson superPerson = null;
+	
+	private SuperPerson(String name, int age) {
+		super(name, age);
+		// TODO Auto-generated constructor stub
+	}
+
+	public static SuperPerson getInstance() {
+		if (superPerson == null) {
+			superPerson = new SuperPerson( "private person", 4);
+		}
+		return superPerson;
+	}
+	
+	int colarSize;
+}
