0a5da36cab83140050cc8477856e7f63cc1eb75e
jennifer.godman@udsl.com <jennifer.godman@udsl.com>
2022-06-17 16:01:10 +0100

    .ArrayListForLoop

---
+++ src/com/qa/ArrayListForLoop/ArrayListForLoop.java
@@ -0,0 +1,28 @@
+package com.qa.ArrayListForLoop;
+
+public class ArrayListForLoop {
+
+
+	public static void main (String[] args) {
+		
+		mathod2();
+	}
+	public static void mathod2(){
+
+	 int num[] = {1, 2, 1, 3, 43, 9, 8, 56, 89,65};{
+		
+	 for (int i = 0; i < num.length; i++) {
+		 num [i] = i + 1;
+		 System.out.println(num[i]);
+		}
+		// TODO Auto-generated method stub
+
+	
+	for (int j = 0; j < num.length; j++) {
+		num[j] = j* 10;
+		    	
+		System.out.println(num[j]);
+		}
+		}
+	}
+}
