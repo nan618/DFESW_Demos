d40b9cb9eb51fcd01a5992bb72cb26c05327794c
jennifer.godman@udsl.com <jennifer.godman@udsl.com>
2022-06-21 10:15:26 +0100

    correct one Iteration

--- src/com/qa/Iteration/Iteration.java
+++ src/com/qa/Iteration/Iteration.java
@@ -2,29 +2,25 @@ package com.qa.Iteration;
 
 public class Iteration {
 
-	public static long main(String[] args) {
+	public static void main(String[] args) {
 		 {
 	
 		 for(int A = 100; A <=200; A++) {
 	          
-	            
-	            if(A > 200) {
-	               
-	      
-	            System.out.println(A);}
-	            else {
-	            	 
-	                    break;}
+	        System.out.println(A);
 		 }
+	
 	                     
+	}
+	}
 	}
 
 		
 		
-		return int A;
-	}
 	
-}
+	
+	
+
 
 
 
---
+++ src/com/qa/stringManipulation/StringManipulation.java
@@ -0,0 +1,14 @@
+package com.qa.stringManipulation;
+
+public class StringManipulation {
+
+	public static void main(String[] args) {
+		String str1 = "yesterday it was raining";
+		String str2 = "today it is sunny";
+		String thirdString = str1 +" " + str2;
+		System.out.println(thirdString); 
+	
+
+	}
+
+}
