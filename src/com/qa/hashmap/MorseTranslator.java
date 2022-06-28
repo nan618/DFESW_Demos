1743f7ddffa4cc7e281dfd2ca11b4dd0694f4d70
jennifer.godman@udsl.com <jennifer.godman@udsl.com>
2022-06-28 14:37:41 +0100

    hashmap

--- src/com/qa/hashmap/MorseTranslator.java
+++ src/com/qa/hashmap/MorseTranslator.java
@@ -1,5 +1,24 @@
 package com.qa.hashmap;
 
-public class MorseTranslator {
+import java.util.*;
+import java.util.stream.Collectors;
 
+public class MorseTranslator {
+	private final Map<String, String> MORSE_MAP;
+
+	public MorseTranslator() {
+		this.MORSE_MAP = new HashMap<>();
+
+		this.MORSE_MAP.put(".-", "a");
+		// ...
+		// ...
+		// ...
+		this.MORSE_MAP.put("--..", "z");
+
+		this.MORSE_MAP.put("/", " ");
+	}
+
+	public String translate(String input) {
+		return Arrays.stream(input.split(" ")).map(n -> this.MORSE_MAP.get(n)).collect(Collectors.joining(""));
+	}
 }
